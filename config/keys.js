a1fb95a092747163bc7268feb22c8ee782408520
Kosam Omollo <hnkhosamomollo3@gmail.com>
2019-05-11 17:05:10 +0300

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://sam:sam1234@sbcluster-u5xar.mongodb.net/test?retryWrites=true',
+  googleClientID: '452202856773-ffij1gend4dn0icp1fdm6h0b15j8oik7.apps.googleusercontent.com',
+  googleClientSecret: 'oEFMxCh5tp9739HRwyo823YR'
+}
